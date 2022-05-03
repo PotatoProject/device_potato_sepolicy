0efa5915dfd9d0b03813a8e85682f7b7a94dbf78
SahilSonar <sss.sonar2003@gmail.com>
2022-05-03 07:57:17 +0200

    sepolicy: Include flipendo and turbo_adapter policy
    Change-Id: Id384d5894c4b440676169034d920f09f5478d336

--- common/sepolicy.mk
+++ common/sepolicy.mk
@@ -18,11 +18,15 @@ SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
 ifeq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
     device/potato/sepolicy/common/dynamic \
-    device/potato/sepolicy/common/system
+    device/potato/sepolicy/common/system \
+    hardware/google/pixel-sepolicy/flipendo \
+    hardware/google/pixel-sepolicy/turbo_adapter
 else
 BOARD_VENDOR_SEPOLICY_DIRS += \
     device/potato/sepolicy/common/dynamic \
-    device/potato/sepolicy/common/vendor
+    device/potato/sepolicy/common/vendor \
+    hardware/google/pixel-sepolicy/flipendo \
+    hardware/google/pixel-sepolicy/turbo_adapter
 endif
 
 # Selectively include legacy rules defined by the products
